48471925df0bbf75e0955efb22ed0e128222ada4
Artur <98638523+Arturrr13@users.noreply.github.com>
2025-01-12 16:21:07 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import App from './App.vue'
 
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-ScrollTrigger.config ({ autoRefreshEvents : "DOMContentLoaded,load,visibilitychange" }); 
 gsap.registerPlugin(ScrollTrigger)
+ScrollTrigger.normalizeScroll(true)
+ScrollTrigger.config({autoRefreshEvents: "DOMContentLoaded,load,visibilitychange"});
 
 createApp(App).use(router).mount('#app')
